c324b53c2aa735b96723438d0b54b345528c6dbf
alpha15530 <alphalufa@gmail.com>
2017-12-18 16:02:06 +0900

    initial commit

---
+++ gulpfile.js
@@ -0,0 +1,81 @@
+require('es6-promise').polyfill();
+
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cleanCSS = require('gulp-clean-css');
+var babel = require('gulp-babel');
+var uglify = require('gulp-uglify');
+var pump = require('pump');
+var del = require('del');
+var runSequence = require('run-sequence');
+
+gulp.task('clean:public', function() {
+    del('public');
+});
+
+gulp.task('clean:css', function() {
+    del('public/css/*.css');
+});
+
+gulp.task('clean:js', function() {
+    del('public/js/*.js');
+});
+
+gulp.task('scss-compile', function(cb) {
+    pump([
+        gulp.src('./dev/style/style.scss'),
+        sass.sync({
+            outputStyle: 'compressed'
+        }).on('error', sass.logError),
+        gulp.dest('./public/css/')
+    ], cb);
+});
+
+gulp.task('babel', function(cb) {
+    pump([
+        gulp.src('./dev/scripts/**/*.js'),
+        babel({
+            "presets": ['es2015']
+        }),
+        //uglify(),
+        gulp.dest('./public/js/')
+    ], cb);
+});
+
+gulp.task('build:css', function() {
+    return runSequence(
+        'clean:css',
+        'scss-compile'
+    );
+});
+
+gulp.task('build:js', function() {
+    return runSequence(
+        'clean:js',
+        'babel'
+    );
+});
+
+gulp.task('watch', function(cb) {
+    gulp.watch([
+        './dev/style/*.scss'
+    ], [
+        'sass-compile'
+    ]);
+});
+
+gulp.task('default', [
+    'clean',
+    'build:css',
+    'build:js',
+]);
+
+gulp.task('dev', [
+    'compass'
+]);
+
+gulp.task('prod', [
+    'clean:dist',
+    'compass',
+    'babel'
+]);
